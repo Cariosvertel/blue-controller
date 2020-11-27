e16126678deb009a48ce706f24b68b15952f1df3
Carlos Rios <cariosvertel@gmail.com>
2020-11-26 20:18:16 -0500

    initial commit

---
+++ Sources/SamplePackage/BlueViewController.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carlos Vertel on 26/11/20.
+//
+
+import Foundation
+#if canImport(UIKit)
+import UIKit
+
+public class BlueViewController:UIViewController{
+  
+  
+}
+#endif
