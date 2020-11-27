2153adf1f676466ca141dbeda67732f62c9bbfb0
Carlos Rios <cariosvertel@gmail.com>
2020-11-26 21:09:40 -0500

    now with storyboard

---
+++ Sources/SamplePackage/GreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carlos Vertel on 26/11/20.
+//
+
+import Foundation
+#if canImport(UIKit)
+import UIKit
+class GreenViewController:UIViewController{
+  
+  public class func loadController()->GreenViewController?{
+    let sb = UIStoryboard(name:"GreenViewController", bundle:Bundle.module)
+    return sb.instantiateInitialViewController() as? GreenViewController
+  }
+}
+
+#endif
